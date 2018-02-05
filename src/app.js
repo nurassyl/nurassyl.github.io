fc5c306a71d6bc7a68c5fc5c0bb705ddf626a3ed
Nurasyl Aldan <nurassyl.aldan@gmail.com>
2018-02-05 21:02:07 +0600

    Cyan color header created.

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ class App extends Component {
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                     <div className="container">
                         <a className="navbar-brand" href="#">
-                            Nurassyl
+                            &lt;Nurassyl /&gt;
                         </a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
@@ -30,25 +30,31 @@ class App extends Component {
                                 <li className="nav-item">
                                     <a className="nav-link" href="#">
                                         <i className="fa fa-child"></i>
-                                        <span>Bio</span>
+                                        <span>Биография</span>
                                     </a>
                                 </li>
                                 <li className="nav-item">
                                     <a className="nav-link" href="#">
                                         <i className="fa fa-briefcase"></i>
-                                        <span>Portfolio</span>
+                                        <span>Портфолио</span>
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">
+                                        <i className="fa fa-code"></i>
+                                        <span>Технологии</span>
                                     </a>
                                 </li>
                                 <li className="nav-item">
                                     <a className="nav-link" href="#">
                                         <i className="fa fa-address-card"></i>
-                                        <span>Resume</span>
+                                        <span>Резюме</span>
                                     </a>
                                 </li>
                                 <li className="nav-item">
                                     <a className="nav-link" href="#">
                                         <i className="fa fa-phone"></i>
-                                        <span>Contacts</span>
+                                        <span>Контакты</span>
                                     </a>
                                 </li>
                             </ul>
@@ -56,7 +62,37 @@ class App extends Component {
                     </div>
                 </nav>
                 <header>
-                    <h2 className="text-white text-center">Front-end developer</h2>
+                    <div className="container">
+                        <h1 className="text-white text-center">
+                            <i className="fa fa-code"></i> Front-end разработчик <i className="fa fa-code"></i>
+                        </h1>
+                        <div className="row">
+                            <div className="col text-center text-white">
+                                <p>
+                                    <i className="fa fa-hourglass"></i>
+                                </p>
+                                <p className="text">
+                                    <b>3 года</b> опыт работы
+                                </p>
+                            </div>
+                            <div className="col text-center text-white">
+                                <p>
+                                    <i className="fa fa-trophy"></i>
+                                </p>
+                                <p className="text">
+                                    Опыт <b>Middle</b> уровень
+                                </p>
+                            </div>
+                            <div className="col text-center text-white">
+                                <p>
+                                    <i className="fa fa-search"></i>
+                                </p>
+                                <p className="text">
+                                    Данный момент <b>ищу работу</b>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </header>
             </div>
         );
