b83b311374e80978a9a4c71ecd7d09a325f3677c
Nurasyl Aldan <nurassyl.aldan@gmail.com>
2018-02-06 04:53:58 +0600

    Resume created.

--- src/app.js
+++ src/app.js
@@ -96,8 +96,8 @@ class App extends Component {
                     </div>
                 </header>
                 <main class="container">
-                    <div id="biography" className="page">
-                        <h3>Биография</h3>
+                    <div id="biography" className="reference">
+                        <h3 className="title">Биография</h3>
                         <p className="text-indent">
                             Моё имя Нурасыл, полностью Нурасыл Алданов Нургазиевич.
                             Я родился 21 ноября 1996 года в городе Байконур, Казахстан.
@@ -105,7 +105,7 @@ class App extends Component {
                             Живу в городе Байконур с родителями.
                             Работаю фрилансером.
                         </p>
-                        <h6>Знакомство с компьютером</h6>
+                        <h6 className="subtitle">Знакомство с компьютером</h6>
                         <p className="text-indent">
                             Я вырос в городе Байконур. Первый раз с компьютером познакомился в середине 8 класса (2010 год).
                             Большое спасибо родителям за то, что купили мне компьютер.
@@ -119,7 +119,7 @@ class App extends Component {
                             С этого момента я заинтересовался программными обеспечениями.
                             Изучал MS Office (Word, Excel, PowerPoint), Photoshop, Audition, CorelDraw и т. д.
                         </p>
-                        <h6>Образование</h6>
+                        <h6 className="subtitle">Образование</h6>
                         <p className="text-indent">
                             Пошел в школу в 2003 году и закончил 9 класс в 2012 году по имени "К. Э. Циолковский" в городе Байконур.
                             Поступил в колледж по имени "Жамбылский политехнический колледж" в городе Тараз по специальности "Программист" и закончил в 2016 году.
@@ -130,7 +130,7 @@ class App extends Component {
                                 Мои работы есть в моих профилах на <a href="http://github.com/nurassyl" target="__blank">github.com</a>, <a href="http://bitbucket.org/nurassyl" target="__blank">bitbucket.org</a>, <a href="http://behance.net/nurasyl" target="__blank">behacne.net</a> и в <a href="#portfolio" target="__blank">портфолио</a>
                             </span>
                         </p>
-                        <h6>Программирование</h6>
+                        <h6 className="subtitle">Программирование</h6>
                         <p className="text-indent">
                             В один прекрасный день сидел в социальной сети "ВКонтакте" и возник вопрос "Как создаются социальные сети и сайты?".
                             С этого момента я начал искать в интернете информации о сайтах и о их созданиях.
@@ -157,6 +157,166 @@ class App extends Component {
                         <p></p>
                     </div>
                     <hr />
+                    <div id="resume" className="reference">
+                        <h3 className="title">Резюме</h3>
+                        <h5 className="text-uppercase">
+                            <b>
+                                <i className="fa fa-id-card-o"></i> Нурасыл Алданов Нургазиевич
+                            </b>
+                        </h5>
+                        <h5 className="subtitle">
+                            Front-end разработчик
+                        </h5>
+                        <hr className="bold" />
+                        <table className="table table-striped">
+                            <tr>
+                                <td>
+                                    <b>E-mail:</b>
+                                </td>
+                                <td>
+                                    <a href="mailto:nurassyl.aldan@gmail.com">nurassyl.aldan@gmail.com</a> (предпочитаемый способ связи)
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Телефон номер:</b>
+                                </td>
+                                <td>
+                                    <a href="tel:+7 (775) 380 0405">+7 (775) 380 0405</a> (доступно WhatsApp и Telegram)
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Skype:</b>
+                                </td>
+                                <td>
+                                    <a href="skype:nurassyl.aldan">nurassyl.aldan</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Официальный сайт:</b>
+                                </td>
+                                <td>
+                                    <a href="http://nurassyl.github.io" target="__blank">nurassyl.github.io</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Дата рождения:</b>
+                                </td>
+                                <td>
+                                    21.11.1996 (21 лет)
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Место рождения:</b>
+                                </td>
+                                <td>
+                                    город <a href="https://ru.wikipedia.org/wiki/%D0%91%D0%B0%D0%B9%D0%BA%D0%BE%D0%BD%D1%83%D1%80_(%D0%B3%D0%BE%D1%80%D0%BE%D0%B4)" target="__blank">Байконур</a>, <a href="https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D0%B7%D0%B0%D1%85%D1%81%D1%82%D0%B0%D0%BD" target="__blank">Казахстан</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Пол:</b>
+                                </td>
+                                <td>
+                                    мужской
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Место жительства:</b>
+                                </td>
+                                <td>
+                                    город <a href="https://ru.wikipedia.org/wiki/%D0%91%D0%B0%D0%B9%D0%BA%D0%BE%D0%BD%D1%83%D1%80_(%D0%B3%D0%BE%D1%80%D0%BE%D0%B4)" target="__blank">Байконур</a>, <a href="https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D0%B7%D0%B0%D1%85%D1%81%D1%82%D0%B0%D0%BD" target="__blank">Казахстан</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Зарплатные ожидания:</b>
+                                </td>
+                                <td>
+                                    40 000 рублей в месяц
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>График и режим работы:</b>
+                                </td>
+                                <td>
+                                    Удаленная и полный рабочий день
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Опыт работы:</b>
+                                </td>
+                                <td>
+                                    3 года, фрилансер.
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Языки:</b>
+                                </td>
+                                <td>
+                                    <a href="https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D0%B7%D0%B0%D1%85%D1%81%D0%BA%D0%B8%D0%B9_%D1%8F%D0%B7%D1%8B%D0%BA" target="__blank">Казахский</a> (родной), Русский (свободно владею), Английский (Pre-Intermediate)
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Обо мне:</b>
+                                </td>
+                                <td>
+                                    Являюсь front-end разработчиком.
+                                    Готов работать с Agile/Scrum методологиями разработки.
+                                    Очень хорошо понимаю работы браузеров, HTTP серверов, движков.
+                                    <br />
+                                    HTML & CSS движки: Webkit, Gecko, Presto, Edge и т.д.
+                                    <br />
+                                    JavaScript движки: V8, SpiderMonkey.
+                                    <br />
+                                    Соблюдаю правила стандартов W3C, EcmaScript.
+                                    <br />
+                                    Защищаю сайты от атак XSS/CSRF/XSRF.
+                                    Знаю HTTP заголовки и CORS заголовки.
+                                    <br />
+                                    Оптимизирую веб страниц по метатегам и SEO стандартам для правильного индексирования поисковыми системами Google, Yandex, Bing, Yahoo и т.д.
+                                    <br />
+                                    Клиентскую часть программирую на языке программирования JavaScript со стандартом EcmaScript 6.
+                                    <br />
+                                    Верстаю кроссбраузерно, адаптивно, семантично с/без PSD макета и с системами Grid system и Flexbox.
+                                    <br />
+                                    Создаю SPA и MPA приложений.
+                                    <br /><br />
+                                    <b>Использую:</b><br />
+                                    Git - для систем конроля версий.<br />
+                                    Github - для основного репозитория проектов.<br />
+                                    Для сервера: NodeJS.<br />
+                                    Пакетные менеджеры: NPM, Yarn, Bower.<br />
+                                    Unit-тестирование: Jasmine, Karma, Mocha с методами TDD и BDD.<br />
+                                    Транспилятор: Babel для старых браузеров или/и для браузеров, которые не поддерживают стандарта EcmaScript.<br />
+                                    Сборщики и таск-менеджеры: Webpack, Gulp, Grunt.<br />
+                                    Фреймворк jQuery: Для манипуляции с DOM, BOM.<br />
+                                    AJAX: XMLHttpResponse, Axios, jQuery AJAX, Angular HTTP object.
+                                    Для нормализация CSS тилей: normalize.css<br />
+                                    CSS препроцессоры: LESS, SASS/SCSS.<br />
+                                    CSS методологии: БЭМ.<br />
+                                    HTTP API: RESTful(CRUD) API, JWT, JSON/JSON5.
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Ключевые навыки:</b>
+                                </td>
+                                <td>
+                                    Linux/GNU, BashScript, Git, Github, Markdown, HTML5/XHTML, XML, CSS3, ES6, JSON5, Webpack, Babel, ReactJS, Redux, AngularJS, Angular, VueJS, Vuex, Axios, jQuery, REST API и т. д.
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
                 </main>
             </div>
         );
